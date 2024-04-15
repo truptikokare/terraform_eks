073cdf938773f53d565094eb4ff651570e71567a
AR Shankar <valaxytech@gmail.com>
2024-04-15 19:30:09 +0530

    updated instance info

--- eks/eks.tf
+++ eks/eks.tf
@@ -118,7 +118,7 @@ resource "aws_iam_instance_profile" "worker" {
 
 ###############################################################################################################
 resource "aws_eks_cluster" "eks" {
-  name = "ed-eks-01"
+  name = "valaxy-dev"
   role_arn = aws_iam_role.master.arn
 
   vpc_config {
@@ -142,9 +142,9 @@ resource "aws_eks_node_group" "frontend" {
   node_role_arn   = aws_iam_role.worker.arn
   capacity_type = "ON_DEMAND"
   disk_size = "20"
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
   remote_access {
-    ec2_ssh_key = "rtp-03"
+    ec2_ssh_key = "terraform"
     source_security_group_ids = [var.sg_ids]
   } 
 
@@ -174,39 +174,3 @@ resource "aws_eks_node_group" "frontend" {
     #aws_subnet.pub_sub2,
   ]
 }
-
-resource "aws_eks_node_group" "backend" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "dev"
-  node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids = [var.subnet_ids[0],var.subnet_ids[1]]
-  capacity_type = "ON_DEMAND"
-  disk_size = "20"
-  instance_types = ["t3.medium"]
-  remote_access {
-    ec2_ssh_key = "rtp-03"
-    source_security_group_ids = [var.sg_ids]
-  } 
-  
-  labels =  tomap({env = "dev"})
-  
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    #aws_subnet.pub_sub1,
-    #aws_subnet.pub_sub2,
-  ]
-}
-
-
